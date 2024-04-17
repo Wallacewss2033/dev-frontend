cbe62b29a172bdd3520c692e2f1b5ddde2546a70
Wallacewss2033 <wallace-2033@hotmail.com>
2024-04-17 15:45:13 -0300

    feat: criar itens do header

---
+++ src/components/NavItem/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+
+interface NavItemPros {
+  text: string;
+  drop: boolean;
+}
+
+const NavItem: React.FC<NavItemPros> = ({ text, drop }) => {
+  return (
+    <li className="nav-item">
+      <a href="#" className="nav-link text-align custom">
+        <span className="mx-1">{text}</span>
+        {!!drop && <i className="bi bi-chevron-down icon"></i>}
+      </a>
+    </li>
+  );
+};
+
+export default NavItem;
