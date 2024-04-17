ef96d232095b912727c91716a79c95e3ff9a274d
Wallacewss2033 <wallace-2033@hotmail.com>
2024-04-17 20:26:33 -0300

    feat: criar butão customizável

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import React, { ButtonHTMLAttributes } from "react";
+import "./styles.css";
+
+interface ButtonInterface
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  btnType: "outline-white" | "success" | "white";
+  radius?: number;
+}
+
+const Button: React.FC<ButtonInterface> = ({
+  btnType,
+  children,
+  radius,
+  ...props
+}) => {
+  return (
+    <button
+      style={{ borderRadius: radius }}
+      className={`btn btn-${btnType}`}
+      {...props}
+    >
+      <div className="d-flex justify-content-evenly align-items-center">
+        {children}
+      </div>
+    </button>
+  );
+};
+
+export default Button;
