52aa9aa3ef93459c4fb8e7ac73ea591f5420de69
Wallacewss2033 <wallace-2033@hotmail.com>
2024-04-17 23:50:45 -0300

    feat: criar componente de lista de categorias

---
+++ src/components/CategoryList/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+interface CategoryInterface {
+    categories: Array<string>;
+}
+const CategoryList: React.FC<CategoryInterface> = ({categories}) => {
+
+  return (
+    <p className="mb-5">
+      {categories.map((category, index) => {
+        if (index !== categories.length - 1) {
+          return (
+            <span className="text-white">
+              <span className="text-aquamarine mx-2">{` ${category} `}</span>/
+            </span>
+          );
+        }
+
+        return <span className="text-white mx-2">{`${category}`}</span>;
+      })}
+    </p>
+  );
+};
+
+export default CategoryList;
